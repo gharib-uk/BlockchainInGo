ec2b9544d443a0167afd9eff5e525ca8a9e5f5f0
Alireza Gharib <alirezagharib110@gmail.com>
2025-08-20 19:54:25 +0330

    refactor project into new structure

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,13 +1,13 @@
 package wallet
 
 import (
+	"BlockchainInGo/utils"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/json"
 	"fmt"
-	"math/big"
 
 	"github.com/btcsuite/btcutil/base58"
 	"golang.org/x/crypto/ripemd160"
@@ -19,7 +19,6 @@ type Wallet struct {
 	blockchainAddress string
 }
 
-// NewWallet create private-key, public-key and blockchainAddress based on Bitcoin version 1 address
 func NewWallet() *Wallet {
 	// 1. Creating ECDSA private key (32 bytes) public key (64 bytes)
 	w := new(Wallet)
@@ -48,11 +47,11 @@ func NewWallet() *Wallet {
 	h6.Write(digest5)
 	digest6 := h6.Sum(nil)
 	// 7. Take the first 4 bytes of the second SHA-256 hash for checksum.
-	chsum := digest6[:4]
+	chkSum := digest6[:4]
 	// 8. Add the 4 checksum bytes from 7 at the end of extended RIPEMD-160 hash from 4 (25 bytes).
 	dc8 := make([]byte, 25)
 	copy(dc8[:21], vd4[:])
-	copy(dc8[21:], chsum[:])
+	copy(dc8[21:], chkSum[:])
 	// 9. Convert the result from a byte string into base58.
 	address := base58.Encode(dc8)
 	w.blockchainAddress = address
@@ -92,11 +91,11 @@ func NewTransaction(privateKey *ecdsa.PrivateKey, publicKey *ecdsa.PublicKey,
 	return &Transaction{privateKey, publicKey, sender, recipient, value}
 }
 
-func (t *Transaction) GenerateSignature() *Signature {
+func (t *Transaction) GenerateSignature() *utils.Signature {
 	m, _ := json.Marshal(t)
 	h := sha256.Sum256(m)
 	r, s, _ := ecdsa.Sign(rand.Reader, t.senderPrivateKey, h[:])
-	return &Signature{r, s}
+	return &utils.Signature{R: r, S: s}
 }
 
 func (t *Transaction) MarshalJSON() ([]byte, error) {
@@ -109,13 +108,4 @@ func (t *Transaction) MarshalJSON() ([]byte, error) {
 		Recipient: t.recipientBlockchainAddress,
 		Value:     t.value,
 	})
-}
-
-type Signature struct {
-	R *big.Int
-	S *big.Int
-}
-
-func (s *Signature) String() string {
-	return fmt.Sprintf("%x%x", s.R, s.S)
 }
